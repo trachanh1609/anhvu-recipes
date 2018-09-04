7e807959114de00c3d1dbbe983f885f0bfda4992
Vu Nguyen <trachanh@gmail.com>
2018-09-04 15:20:09 +0300

    config Apollo client to frontend

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,16 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:5000/graphql'
+})
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+, document.getElementById('root'));
 registerServiceWorker();
