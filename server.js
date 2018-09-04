68538cbf69fe26484f5550859c9f8e020e97b337
Vu Nguyen <trachanh@gmail.com>
2018-09-04 14:49:33 +0300

    GraphQL backend working

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 require('dotenv').config({path: 'variables.env'});
 
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
+
+
+// Bringing GraphQL middlewares
+const {graphiqlExpress, graphqlExpress} = require('apollo-server-express');
+const {makeExecutableSchema} = require('graphql-tools');
+
+const {typeDefs} = require('./schema');
+const { resolvers } = require('./resolvers');
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+})
+
+// Connect to Mongo DB
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
     console.log('DB connected');
@@ -13,6 +31,21 @@ mongoose.connect(process.env.MONGO_URI)
 })
 
 const PORT = process.env.PORT || 5000 ;
+
+// Create GraphQL Application
+app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'})) ;
+
+// Connect Schemas with GraphQL
+app.use(
+    '/graphql',
+    bodyParser.json(),
+    graphqlExpress({
+    schema,
+    context: {
+        Recipe,
+        User
+    }
+}))
 
 app.listen(PORT, ()=> {
     console.log('Server listening on port', PORT);
