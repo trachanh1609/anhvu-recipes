b909bff1d25e55cd5cc6f95d7f6bdcaae444d178
Vu Nguyen <trachanh@gmail.com>
2018-09-05 13:29:35 +0300

    Create Signin Mutation on Backend

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const createToken = (user, secret, expiresIn) => {
     const {username, password} = user ;
@@ -40,6 +41,22 @@ exports.resolvers = {
             }).save();
 
             return { token: createToken( newUser, process.env.SECRET , '1hr' )}
+        },
+
+        signinUser: async( root, { username , password}, { User}) => {
+            const user = await User.findOne({ username });
+            if( !user ) {
+                throw new Error('User DOES NOT exist');
+            }
+
+            const isValidPassword = await bcrypt.compare(password, user.password);
+            if( !isValidPassword ) {
+                throw new Error('Invalid Password');
+            }
+
+            return { token: createToken( user, process.env.SECRET , '1hr' )}
+
         }
+
     }
 }
