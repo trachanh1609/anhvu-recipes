b36b317c3e1e54b330a02179107d8f6ce6dd25ca
Vu Nguyen <trachanh@gmail.com>
2018-09-05 16:18:57 +0300

    update to see if password needed when pushing

--- client/src/components/Auth/SignUp.js
+++ client/src/components/Auth/SignUp.js
@@ -33,7 +33,6 @@ class SignUp extends React.Component {
 
     validateForm = () => {
         const { username, email, password, passwordConfimation } = this.state ;
-
         const isInvalid = !username || !email || !password || password !== passwordConfimation ;
 
         return isInvalid ;
