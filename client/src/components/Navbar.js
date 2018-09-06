1090f0a21ac3f31b1aac9171ec4ccf976219b5f1
Vu Nguyen <trachanh@gmail.com>
2018-09-06 13:30:29 +0300

    Add Navbar Component, Search Component

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => (
+    <nav>
+        <NavbarUnAuth></NavbarUnAuth>
+    </nav>
+);
+
+const NavbarUnAuth = () => (
+<ul>
+    <li>
+        <NavLink to="/" exact>Home</NavLink>
+    </li>
+    <li>
+        <NavLink to="/search" >Search</NavLink>
+    </li>
+    <li>
+        <NavLink to="/signin">SignIn</NavLink>
+    </li>
+    <li>
+        <NavLink to="/signup">SignUp</NavLink>
+    </li>
+</ul>
+)
+
+export default Navbar;
--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -11,7 +11,8 @@ import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-d
 import SignIn from './components/Auth/SignIn';
 import SignUp from './components/Auth/SignUp';
 import withSession from './components/withSession';
-
+import Navbar from './components/Navbar';
+import Search from './components/Recipe/Search';
 
 const client = new ApolloClient({
     uri: 'http://localhost:5000/graphql',
@@ -36,12 +37,16 @@ const client = new ApolloClient({
 
 const Root = ({ refetch }) => (
     <Router>
-        <Switch>
-            <Route path="/" exact component={App} />
-            <Route path="/signin" render={() => <SignIn refetch={refetch} /> } />
-            <Route path="/signup" render={() => <SignUp refetch={refetch} /> } />
-            <Redirect to="/" />
-        </Switch>
+        <Fragment>
+            <Navbar></Navbar>
+            <Switch>
+                <Route path="/" exact component={App} />
+                <Route path="/search" exact component={Search} />
+                <Route path="/signin" render={() => <SignIn refetch={refetch} /> } />
+                <Route path="/signup" render={() => <SignUp refetch={refetch} /> } />
+                <Redirect to="/" />
+            </Switch>
+        </Fragment>
     </Router>
 )
 
