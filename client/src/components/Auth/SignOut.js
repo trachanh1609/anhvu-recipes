9121efa002192e350af552d4a067a26d9bd1b8bf
Vu Nguyen <trachanh@gmail.com>
2018-09-06 14:52:11 +0300

    Implement SignOut functionality

--- client/src/components/Auth/SignOut.js
+++ client/src/components/Auth/SignOut.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { withRouter} from 'react-router-dom';
+import { ApolloConsumer } from 'react-apollo';
 
-const Signout = () => (
-    <button>Signout</button>
+const handleSignOut = (client, history ) => {
+    localStorage.setItem('token', '');
+    client.resetStore();
+    history.push("/");
+}
+const Signout = ({history}) => (
+    <ApolloConsumer>
+        {
+            client => {
+
+                return (
+                    <button onClick={()=> handleSignOut(client, history )}>Signout</button>
+                )
+            }
+        }
+    </ApolloConsumer>
 )
 
-export default Signout;
+export default withRouter(Signout);
